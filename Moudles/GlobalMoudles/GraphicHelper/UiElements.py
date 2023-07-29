127dad644538e1e3b148447300b43763f9042cb2
Zi-Max <katakuricharlottesama12@gmail.com>
2023-07-29 12:46:10 +0000

    Update UiElements Change all elements structure
    added base class PygameBase that all elements inhert from
    so that you can avoid unnecessary attributes
    added createText&createBorder methods to it
    because not all elements have border or text (avoid unwanted attrs)
    update all object __init__ method to match the new changes

--- Moudles/GlobalMoudles/GraphicHelper/UiElements.py
+++ Moudles/GlobalMoudles/GraphicHelper/UiElements.py
@@ -3,31 +3,13 @@ from UiFunctions import *
 
 ######################## Main Element ##############################
 ####################################################################
-class PyRect(UiDesgin):
-    def __init__(self, group, name, size, pos, grab="", uiPrefix="", frame=""):
+class PygameBase(UiDesgin):
+    def __init__(self, group, name, ui_prifix=""):
         self.name = name
-        self.type = uiPrefix
+        self.type = ui_prifix
         self.group = group
 
         self.checkInc()
-
-        self.attObj = None
-        self.setFrame(frame)
-
-        self.rect = super().setGeo(size, pos, grab)
-        self.border_rect = super().setGeo(size, pos, grab)
-
-        self.ren_text, self.rect_text = super().setText(self.rect)
-        self.text_align = "center"
-
-        self.active_color = "#FFFFFF"
-
-        self.border_width = 0
-        self.border_color = "#000000"
-        super().setBorder(self.border_width, self.border_color)
-
-        self.text_margin_x = 0
-        self.text_margin_y = 0
 
         self.layer = 1
 
@@ -42,25 +24,48 @@ class PyRect(UiDesgin):
             self.name = f"{inc_dict[self.group]}{self.name}"
             inc_dict[self.group] += 1
 
-    def setFrame(self, frame):
-        if frame == "":
-            self.frame = None
-        else:
-            self.frame = draw_dict[self.group][frame]
-            draw_dict[self.group][frame].children.append(self)
+    def createBorder(self):
+        self.border_rect = pygame.Rect((self.rect.x, self.rect.y), self.rect.size)
+        self.border_width = 0
+        self.border_color = "#FFFFFF"
+        super().setBorder(self.border_width, self.border_color)
+
+    def createText(self):
+        self.ren_text, self.rect_text = super().setText(self.rect)
+        self.text_align = "center"
+
+        self.text_margin_x = 0
+        self.text_margin_y = 0
+
 
 ######################## Derived Elements ##############################
 ########################################################################
 ######################## Pygame Shapes #################################
-class PyCircle(PyRect):
-    def __init__(self, group, name, radius, pos, grab="", frame=""):
-        super().__init__(group, name, (0, 0), pos, grab, "CI", frame)
+class PyRect(PygameBase):
+    def __init__(self, group, name, size, pos, grab=""):
+        super().__init__(group, name, "")
+
+        self.rect = super().setGeo(size, pos, grab)
+
+        self.active_color = "#FFFFFF"
+
+        super().createBorder()
+        super().createText()
+
+class PyCircle(PygameBase):
+    def __init__(self, group, name, radius, pos, grab=""):
+        super().__init__(group, name, group, "CI")
+
+        self.active_color = "#FFFFFF"
+        self.rect = super().setGeo((0, 0), pos, grab)
         self.radius = radius
 
 ######################## My Ui Elements ##############################
-class PyButton(PyRect):
-    def __init__(self, group, name, size, pos, grab="", frame=""):
-        super().__init__(group, name, size, pos, grab, "BT", frame)
+class PyButton(PygameBase):
+    def __init__(self, group, name, size, pos, grab=""):
+        super().__init__(group, name, "BT")
+
+        self.rect = super().setGeo(size, pos, grab)
 
         self.base_color = "#FFFFFF"
         self.active_color = self.base_color
@@ -68,23 +73,26 @@ class PyButton(PyRect):
         self.select_color = "#666666"
         self.disabled_color = "#333333"
 
+        super().createBorder()
+        super().createText()
+
         self.disabled = False
 
-class PyInput(PyRect):
-    def __init__(self, group, name, width, pos, grab="", frame=""):
-        super().__init__(group, name, (width, 0), pos, grab, "IN", frame)
+class PyInput(PygameBase):
+    def __init__(self, group, name, width, pos, grab=""):
+        super().__init__(group, name, "IN")
 
-        self.rect.height = 30
+        self.rect = super().setGeo((width, 1), pos, grab)
+        super().createBorder()
+
         self.text = ""
 
-class PyProgressBar(PyRect):
-    def __init__(self, group, name, width, pos, grab="", uiPrefix="", frame=""):
-        super().__init__(group, name, (0, 0), pos, grab, uiPrefix, frame)
+class PyProgressBar(PygameBase):
+    def __init__(self, group, name, width, pos, grab="", ui_prifix="PR"):
+        super().__init__(group, name, ui_prifix)
 
-        bar_pos = super().setGeo((0, 0), pos, grab, False)
-        bar_fill_pos = super().setGeo((0, 0), pos, grab, False)
-        self.rect = super().setVector((width * 30, 15), bar_pos[0], grab)
-        self.bar_fill_rect = super().setVector((width * 30, 15), bar_fill_pos[0], grab)
+        self.bar = super().setGeo((width, 1), pos, grab)
+        self.bar_fill = super().setGeo((width, 1), pos, grab)
 
         self.start_pos = self.rect.centerx - self.rect.width / 2
         self.end_pos = self.rect.centerx + self.rect.width / 2
@@ -95,16 +103,18 @@ class PyProgressBar(PyRect):
 
         self.disabled = False
 
+        super().createBorder()
+        super().createText()
+
     def setSlider(self, new_precent):
         self.precent = new_precent
         self.bar_fill_rect.width = self.handle_rect.x - self.start_pos
 
 class PySlider(PyProgressBar):
-    def __init__(self, group, name, width, pos, grab="", frame=""):
-        super().__init__(group, name, width, pos, grab, "SL", frame)
-        handle_pos = super().setGeo((0, 0), pos, grab, False)
+    def __init__(self, group, name, width, pos, grab=""):
+        super().__init__(group, name, width, pos, grab, "SL")
 
-        self.handle_rect = super().setVector((15, 20), handle_pos[0], grab)
+        self.handle_rect = super().setGeo((1, 1), pos, grab)
 
         self.handle_color = "#FFFFFF"
 
@@ -113,19 +123,21 @@ class PySlider(PyProgressBar):
         self.handle_rect.x = self.start_pos + (self.rect.width * self.precent)
         self.bar_fill_rect.width = self.handle_rect.x - self.start_pos
 
-class PyFrame(PyRect):
-    def __init__(self, group, name, size, pos, grab):
-        super().__init__(group, name, size, pos, grab, "FR")
+# class PyFrame(PyRect):
+#     def __init__(self, group, name, size, pos, grab):
+#         super().__init__(group, name, size, pos, grab, "FR")
 
-        self.children = []
+#         self.children = []
 
-    def moveChildren(self, x, y):
-        for child in self.children:
-            child.addMargin(x, y)
+#     def moveChildren(self, x, y):
+#         for child in self.children:
+#             child.addMargin(x, y)
 
-class PyMultiArea(PyRect):
+class PyMultiArea(PygameBase):
     def __init__(self, group, name, size, pos, grab, lines, line_height):
-        super().__init__(group, name, size, pos, grab, "MA")
+        super().__init__(group, name, "MA")
+
+        self.rect = super().setGeo(size, pos, grab)
 
         self.line_height = line_height
         self.text_elements = []
